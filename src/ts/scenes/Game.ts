e1ac75926f2c60b27dd1f366f39615e8850d3361
Pavle <pgoloskokovic@gmail.com>
2025-01-25 23:48:01 +0100

    bubble sort

--- src/ts/scenes/Game.ts
+++ src/ts/scenes/Game.ts
@@ -15,6 +15,26 @@ for (let i = 0; i < 100; i++)
     canopy.push(20 + (isUp ? elevation : 1 - elevation) * 70);
 }
 
+const bubbleSort = (arr: number[]): number[] =>
+{
+    const len = arr.length;
+
+    for (let i = 0; i < len - 1; i++)
+    {
+        for (let j = 0; j < len - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+};
+
+console.log(bubbleSort([5, 3, 1, 4, 6]));
+
 /**
  * Game Phaser scene.
  *
