b440cde1f955a2549a93bfdf176bd4420e4469f2
Pavle <pgoloskokovic@gmail.com>
2025-01-25 21:42:25 +0100

    100 bars

--- src/ts/scenes/Game.ts
+++ src/ts/scenes/Game.ts
@@ -14,14 +14,18 @@ export class Game extends Scene {
     {
         console.info('Game enter');
 
-        const scale = this.scale;
-        const x = scale.width / 2;
-        const y = scale.height / 2;
+        // const scale = this.scale;
+        // const x = scale.width / 2;
+        // const y = scale.height / 2;
 
         const add = this.add;
-        add.image(x, y, 'sprites', 'white')
-            .setScale(10);
-        add.image(x, y, 'sprites', 'red')
-            .setScale(4);
+
+        for (let i = 0; i < 100; i++)
+        {
+            add.image(60 + 1 + i * 18, 800, 'sprites', i === 0 ? 'red' : 'white')
+                .setOrigin(0.5, 1)
+                .setDisplaySize(18 - 2, 200 + 5.4 * i);
+
+        }
     }
 }
